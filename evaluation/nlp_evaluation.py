e6fac662ae1e8e332eb29e423eae13f17a90789a
dennisn <denonjugush@gmail.com>
2019-06-10 16:18:06 +0300

    Calculating precision, recall and fscore

--- evaluation/nlp_evaluation.py
+++ evaluation/nlp_evaluation.py
@@ -16,6 +16,8 @@ num_N = retrieved.count('N')
 
 def ex21():
     """
+    Calculate evaluation metrics (Precision, Recall, Fscore) for the
+    'NN' tag.
     """
     correct_tags = 0
     total_tags = 0 # matrix
@@ -26,16 +28,27 @@ def ex21():
     pred_tags = [tag[1] for tag in hmm_tagger.tag(test_tokens)] # predict a tag given a token
     
     # create confusion matrix to evaluate results
-    cmat = ConfusionMatrix(reference=test_tags, test=pred_tags, sort_by_count=True)
-    cmat_tag_dict = {item.split()[0].split(':')[0]: item.split()[1] for item in cmat.key().split('\n')[1:-1]}
-    cmat_tag_list = [item.split()[1] for item in cmat.key().split('\n')[1:-1]]
-    for tag_ref in cmat_tag_list:
-        for tag_test in cmat_tag_list:
+    cm = ConfusionMatrix(reference=test_tags, test=pred_tags, sort_by_count=True)
+    cm_tag_dict = {item.split()[0].split(':')[0]: item.split()[1] for item in cm.key().split('\n')[1:-1]}
+    cm_tag_list = [item.split()[1] for item in cm.key().split('\n')[1:-1]]
+    for tag_ref in cm_tag_list:
+        for tag_test in cm_tag_list:
             if tag_ref == tag_test:
-                correct_tags += cmat[tag_ref, tag_test]
-            total_tags = cmat[tag_ref, tag_test]
+                correct_tags += cm[tag_ref, tag_test]
+            total_tags += cm[tag_ref, tag_test]
     accuracy = correct_tags/total_tags
     print("Overall accuracy of the HMM model: {}".format(accuracy))
+
+    # Calculating Precision, Recall and F-Score for the 'NN' tag
+    true_p = cm['NN', 'NN']
+    false_p = sum([cm[item, 'NN'] for item in cm_tag_list if item != 'NN'])
+    all_true_nn = sum([cm['NN', item] for item in cm_tag_list])  # reference
+    precision = true_p/(true_p + false_p)
+    recall = true_p/all_true_nn
+    f_score = 2 * precision * recall /(precision + recall)
+    print("Precision for 'NN' tag: {}".format(precision))
+    print("Recall score for 'NN' tag {}".format(recall))
+    print("F-score for 'NN' with Beta=1: {}".format(f_score))
 
 if __name__ == '__main__':
 #     true_pos = total_relevant
@@ -52,4 +65,4 @@ if __name__ == '__main__':
 #     print("F-Score with beta=1: {}".format((2*precision*recall)/(precision + recall)))
 #     print("Accuracy: {}".format(num_R/total_docs))
 
-    print(ex21())
+    ex21()
