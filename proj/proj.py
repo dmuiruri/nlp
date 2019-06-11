1e2fd0738bb0b941263d891d4f6b7739d064b0d2
dennisn <denonjugush@gmail.com>
2019-06-11 20:32:31 +0300

    Added first version of topic modelling function

--- proj/proj.py
+++ proj/proj.py
@@ -9,6 +9,9 @@ the corpus with the named entity.
 import spacy
 import gensim
 from os import listdir, path
+from nltk.corpus import stopwords
+from gensim.models import LdaModel
+from gensim import corpora
 
 nlp = spacy.load("en")
 data_dir = path.dirname("./business/")
@@ -42,5 +45,27 @@ def process_ner(entity='Nokia'):
                 print("File was not found or could not be openend")
     return articles_list
 
+def topic_modelling(files=['114.txt', '100.txt', '465.txt', '059.txt']):
+    """
+    perform topic modelling for a given list of files
+    """
+    ntopics = 5
+    articles = []
+    fp = path.join(data_dir, '100.txt')
+    stop_words = set(stopwords.words('english'))
+
+    with open(fp) as f:
+        text = f.read().split()
+    articles.append([word for word in text if word not in stop_words])
+
+    dictionary = corpora.Dictionary(articles)
+    corpus = [dictionary.doc2bow(a) for a in articles]  # doc to BOW
+    lda = LdaModel(corpus, id2word=dictionary, num_topics=ntopics, passes=100)
+    for i in range(ntopics):
+        topwords = lda.show_topic(i, topn=5)
+        print("Top words in topic {}: {}\n".format(i+1, topwords))
+
+
 if __name__ == '__main__':
-    print(process_ner(entity='WorldCom'))
+    # print(process_ner(entity='WorldCom'))
+    topic_modelling()
