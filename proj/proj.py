c022f3ac3a12bc06b1443f2553dd5e3681dd81dc
dennisn <denonjugush@gmail.com>
2019-06-11 19:53:04 +0300

    Cleaned up the NER processing

--- proj/proj.py
+++ proj/proj.py
@@ -1,11 +1,15 @@
 #! /usr/bin/env python
 """
 In this script we determing named entities in the corpus and try to
-identoity the topics coverage in the text.
+identity the topics coverage in the text.
+
+Search for a named entity in the corpus and see which articles are in
+the corpus with the named entity.
 """
 import spacy
 import gensim
 from os import listdir, path
+
 nlp = spacy.load("en")
 data_dir = path.dirname("./business/")
 
@@ -18,20 +22,25 @@ def get_tokens(article):
         for token in sent:
             print(token.text, token.lemma_, token.pos_, token.tag_)
     
-def process_ner():
+def process_ner(entity='Nokia'):
     """
-    Get named entities in an article
+    Search for articles with a given named entity in an article
+
+    Returns a list with files containing the named entity.
     """
-    for article in listdir("./business"):
+    articles_list = []
+    for article in listdir(data_dir):
         fp = path.join(data_dir, article)
         if path.isfile(fp):
             try:
                 with open(fp, 'r') as f:
-                    doc = nlp(f.read())
-                print("\n Named Entities in  article {} ".format(article))
-                print(doc.ents)
+                    temp = f.read()
+                    if temp.lower().find(entity.lower()) >= 0:  # lowercase to find match
+                        articles_list.append(article)
+                        doc = nlp(temp)
             except FileNotFoundError:
                 print("File was not found or could not be openend")
+    return articles_list
 
 if __name__ == '__main__':
-    process_ner()
+    print(process_ner(entity='WorldCom'))
